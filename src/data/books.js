ada3a185c7d459e1854f843b88c626996c7fe75d
AntonioJCS2025 <jadersonc.s@hotmail.com>
2026-08-11 22:21:59 -0300

    adicionando model de livros
    11/08/2026

---
+++ src/data/books.js
@@ -0,0 +1,14 @@
+ const BooksList = [
+        {
+            "id": 1,
+            "title": "Avatar"
+            "author": "Randy Ribay"
+        },
+
+        {
+            id: 2,
+            "title": "Nada pode me ferir"
+            "author": "David Goggins"
+        },
+    ]
+    module.exports = BooksList
---
+++ src/models/bookModel.js
@@ -0,0 +1,56 @@
+const BooksList = require("../data/books")
+
+    const findAll = () => {
+        return BooksList
+    }
+
+    const create = (book) => {
+        BooksList.push(book)
+
+        return book
+    }
+    
+    const findById = (id) => {
+   return BooksList.find(book => book.id === id)     
+
+   }
+
+   const update = (id, data) => {
+    const book = BooksList.find( book => book.id === id)
+
+    if (!book ) return null
+
+    if (data.title){
+            book.title = data.title
+    }
+    return book
+   
+    if (!book ) return null
+
+    if (data.author){
+            book.author = data.author
+    }
+    return book
+
+} 
+
+//    const remove = (id) => {
+//     const index = BooksList.findIndex (user => user.id === id)
+
+//     const deleted = BooksList[index]
+
+//     BooksList.splice(index, 1)
+
+//     if  (index === -1) return null
+
+//     return deleted  
+
+//    }
+
+module.exports = {
+        findAll,
+        create,
+        findById,
+        update,
+       // remove
+    }
